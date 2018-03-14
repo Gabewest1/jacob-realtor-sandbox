691d9e492d2743ded06114b3fa0b0fec1b9f2ae8
Gabriel West <gabe_west1@yahoo.com>
2018-03-14 03:57:03 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -22,13 +22,21 @@ class App extends Component {
         <Navbar1 />
 
         <Header>
-          <HeroImage />
+          <HeroImage src="https://www.youtube.com/embed/EXEiVQ550I8?controls=0&amp;controls=0&amp;mute=1&amp;showinfo=0&amp;rel=0&amp;autoplay=1&amp;loop=1&amp;playlist=EXEiVQ550I8" width="300" height="150" frameborder="0" allowfullscreen="allowfullscreen"></HeroImage>
+          <TintedOverlay />
 
-          <Wrapper>
-            <HeroText>Find Your Next Home</HeroText>
-            <_MLASearchForm />
-          </Wrapper>
+          <HeroContent>
+            <HeroTitle>YOUR NEXT HOME VENTURE STARTS HERE</HeroTitle>
+            <HeroDescription>I Make It Simple</HeroDescription>
+            <HeroButton>Buyers Process</HeroButton>
+            <HeroButton>Sellers Process</HeroButton>
+          </HeroContent>
         </Header>
+
+        <Wrapper>
+          <MLAText>Find Your Next Home</MLAText>
+          <_MLASearchForm />
+        </Wrapper>
 
         <div style={{ padding: "60px 0" }}>
           <Icons />
@@ -46,31 +54,71 @@ class App extends Component {
   }
 }
 
+const HeroTitle = styled.h1`
+
+`
+const HeroDescription = styled.p`
+
+`
+const HeroButton = styled.button`
+  border: solid 2px white;
+  border-radius: 5px;
+  padding: 15px 30px;
+  background: transparent;
+`
+const HeroContent = styled.div`
+  position: absolute;
+  color: white;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  text-align: center;
+
+  ${ HeroButton }:first-of-type {
+    float: left;
+  }
+
+  ${ HeroButton }:last-of-type {
+    float: right;
+  }
+`
+const TintedOverlay = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0,0,0,.7);
+  position: absolute;
+  left: 0;
+  top: 0;
+`
 const _MLASearchForm = styled(MLASearchForm)`
   width: 100%;
 `
-const HeroText = styled.h1`
+const MLAText = styled.h1`
   color: white;
   text-shadow: 2px 2px black;
   margin-bottom: 10px;
+  margin-top: 0;
   font-weight: 100;
   font-family: open sans;
   font-size: 36px;
 `
-const HeroImage = styled.div`
-  background: url(/assets/images/hero.jpg) no-repeat;
-  background-size: cover;
-  background-position: top center;
-  width: 100%;
-  display: block;
+const HeroImage = styled.iframe`
+  width: 300%;
   height: 100vh;
+  position: relative;
+  left: -100%;
+  display: block;
+  border: none;
 `
 const Wrapper = styled.div`
-  position: absolute;
-  bottom: 10%;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 90%; 
+  width: 100%; 
+  padding: 30px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #d81d28;
 `
 const Header = styled.div`
   position: relative;
