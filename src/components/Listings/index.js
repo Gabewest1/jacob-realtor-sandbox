f5cc93b313d1efa6f9c1d618efe0afd0be0f9a2d
Gabriel West <gabe_west1@yahoo.com>
2018-03-17 00:52:35 -0500

    updates listings

--- src/components/Listings/index.js
+++ src/components/Listings/index.js
@@ -5,7 +5,10 @@ export default class Listings extends React.Component {
     render() {
         return (
             <ListingsView>
-                <Header>Listings</Header>
+                <HorizontalBar>
+                    <Header>Listings</Header>
+                </HorizontalBar>
+
                 <List>
                     <ListItem>
                         <Image src="/assets/images/home1.jpg" />
@@ -53,9 +56,14 @@ const ListingsView = styled.div`
     padding: 30px 0;
 `
 const Header = styled.h1`
-    text-align: center;
+    background: white;
+    z-index: 1;
+    position: relative;
+    padding: 0 30px;
     margin-bottom: 40px;
     font-size: 40px;
+    font-weight: 100;
+    display: inline-block;
 `
 const Image = styled.div`
     background: url(${({ src }) => src});
@@ -77,6 +85,13 @@ const Price = styled.span`
 `
 const ListItem = styled.li`
     margin-bottom: 50px;
+    box-shadow: 0px 0px 5px 5px lightgray;
+    transition: all .2s ease-in-out;
+
+    &:hover {
+        box-shadow: 0px 0px 8px 8px lightgray;
+        flex-basis: 32%;
+    }
 `
 const List = styled.ul`
     display: flex;
@@ -87,5 +102,21 @@ const List = styled.ul`
 
     > * {
         flex-basis: 30%;
+    }
+`
+const HorizontalBar = styled.div`
+    position: relative;
+    text-align: center;
+
+    &:after {
+        content: "";
+        background: #5a5959;
+        display: block;
+        position: absolute;
+        height: 1px;
+        width: 80%;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
     }
 `
