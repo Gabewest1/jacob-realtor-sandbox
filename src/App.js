202e94f945b05366b40ef39c48b68703106b4b17
Gabriel West <gabe_west1@yahoo.com>
2018-03-16 21:42:07 -0500

    Added contact

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About2 from "./components/About2"
 import About3 from "./components/About3"
 import About4 from "./components/About4"
 import About5 from "./components/About5"
+import Contact from "./components/Contact"
 import Locations from "./components/Locations"
 import Icons from "./components/Icons"
 import Icons2 from "./components/Icons2"
@@ -62,6 +63,8 @@ class App extends Component {
         <div style={{ paddingTop: 30 }}>
           <About5 />
         </div>
+
+        <Contact />
 
         <Testimonials4 />
 
---
+++ src/components/Contact/index.js
@@ -0,0 +1,124 @@
+import React from "react"
+import styled from "styled-components"
+
+export default class Contact extends React.Component {
+    render() {
+        return (
+            <ContactView>
+                <TintedOverlay />
+                
+                <Header>Let's Talk Real Estate!</Header>
+                <SubHeader>Please use the form below to schedule a call.</SubHeader>
+                <Form>
+                    <FieldView placeholder="First Name" name="first" />
+                    <FieldView placeholder="Last Name" name="last" />
+                    <FieldView placeholder="Phone Number" name="phone" />
+                    <FieldView placeholder="Email" name="email" />
+                    
+                    <Message placeholder="What you'd like to say" />
+                    <SubmitButton>Send Message</SubmitButton>
+                </Form>
+
+            </ContactView>
+        )
+    }
+}
+
+const TintedOverlay = styled.div`
+    height: 100%;
+    width: 100%;
+    background: rgba(0,0,0,.4);
+    position: absolute !important;
+    left: 0;
+    top: 0;
+`
+const Header = styled.h1`
+    font-size: 46px;
+    margin-bottom: 13px;
+`
+const SubHeader = styled.p`
+    margin-top: 0;
+`
+const Message = styled.textarea`
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 7px;
+    border: none;
+    height: 88px;
+    padding: 5px 0 0 7px;
+`
+const SubmitButton = styled.button`
+    width: 54%;
+    margin: 0 auto;
+    display: block;
+    background: #d42235;
+    border: solid thin white;;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    font-family: lato;
+    font-weight: 100;
+    border-radius: 7px;
+    cursor: pointer;
+    outline: none;
+    transition: all .2s ease-in-out;
+
+    &:hover {
+        background: #850513;
+    }
+`
+const Input = styled.input`
+
+`
+const Field = styled.div`
+    width: ${({ name }) => name === "email" ? "100%" : "49%"};
+    display: inline-block;
+
+    label {
+        transform: translateX(100%);
+        display: inline-block;
+    }
+
+    input, textarea {
+        width: 100%;
+        margin: 0;
+        box-sizing: border-box;    
+        border-radius: 7px;
+        border: none;
+        height: 42px;
+        padding-left: 8px;
+    }
+`
+const Form = styled.div`
+    width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    > * {
+        margin-bottom: 20px;
+    }
+
+`
+const ContactView = styled.div`
+    background: url(/assets/images/inside_house.jpg);
+    background-attachment: fixed;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    padding: 30px 0
+    color: white;
+
+    > * {
+        position: relative;
+    }
+`
+const FieldView = props => (
+    <Field>
+        <input { ...props } />
+    </Field>
+)
